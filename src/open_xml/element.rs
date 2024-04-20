3b1bd9c6d5fce3eb8ffe425f856286e3e502b1a7
ndifix <yoshiki.linuxmint@gmail.com>
2024-04-20 23:13:47 +0900

    xmlnsを定義

--- src/open_xml/element.rs
+++ src/open_xml/element.rs
@@ -3,21 +3,24 @@ use std::{fs, io::{self, Write}};
 
 pub struct Types {
   tag: String,
+  xmlns: XmlNamespace,
 }
 
 pub struct Relationships {
   tag: String,
+  xmlns: XmlNamespace,
 }
 
 impl Types {
   pub fn new() -> Types {
     Types {
       tag: "Types".to_string(),
+      xmlns: XmlNamespace::ContentTypes,
     }
   }
 
   pub fn write(&self, writer: &mut io::BufWriter<fs::File>) -> Result<(), io::Error> {
-    write_head_tag(writer, &self.tag)?;
+    write_head_tag(writer, &self.tag, &self.xmlns)?;
 
     write_tail_tag(writer, &self.tag)?;
 
@@ -29,19 +32,22 @@ impl Relationships {
   pub fn new() -> Relationships {
     Relationships {
       tag: "Relationships".to_string(),
+      xmlns: XmlNamespace::Relationships,
     }
   }
 
   pub fn write(&self, writer: &mut io::BufWriter<fs::File>) -> Result<(), io::Error> {
-    write_head_tag(writer, &self.tag)?;
+    write_head_tag(writer, &self.tag, &self.xmlns)?;
     write_tail_tag(writer, &self.tag)?;
 
     Ok(())
   }
 }
 
-fn write_head_tag(writer: &mut io::BufWriter<fs::File>, tag: &str) -> Result<(), io::Error> {
-  let head = String::new() + "<" + tag + ">";
+fn write_head_tag(writer: &mut io::BufWriter<fs::File>, tag: &str, xmlns: &XmlNamespace) -> Result<(), io::Error> {
+  let head = String::new() + "<" + tag;
+  let head = head + " xmlns=\"" + &get_xmlns_string(xmlns) + "\"";
+  let head = head + ">";
   writer.write_all(head.as_bytes())?;
 
   Ok(())
@@ -52,4 +58,18 @@ fn write_tail_tag(writer: &mut io::BufWriter<fs::File>, tag: &str) -> Result<(),
   writer.write_all(tail.as_bytes())?;
 
   Ok(())
+}
+
+enum XmlNamespace {
+  ContentTypes,
+  Presentation,
+  Relationships,
+}
+
+fn get_xmlns_string(xmlns: &XmlNamespace) -> String {
+  match xmlns {
+    XmlNamespace::ContentTypes => "http://schemas.openxmlformats.org/package/2006/content-types",
+    XmlNamespace::Presentation => "http://schemas.openxmlformats.org/presentationml/2006/main",
+    XmlNamespace::Relationships => "http://schemas.openxmlformats.org/package/2006/relationships",
+  }.to_string()
 }
--- src/open_xml/element/presentation.rs
+++ src/open_xml/element/presentation.rs
@@ -1,22 +1,27 @@
 use std::{fs, io};
 
+use super::XmlNamespace;
+
 pub struct Presentation {
   tag: String,
+  xmlns: XmlNamespace,
 }
 
 pub struct PresentationProperties {
   tag: String,
+  xmlns: XmlNamespace,
 }
 
 impl Presentation {
   pub fn new() -> Presentation {
     Presentation {
       tag: "presentation".to_string(),
+      xmlns: XmlNamespace::Presentation,
     }
   }
 
   pub fn write(&self, writer: &mut io::BufWriter<fs::File>) -> Result<(), io::Error> {
-    super::write_head_tag(writer, &self.tag)?;
+    super::write_head_tag(writer, &self.tag, &self.xmlns)?;
 
     super::write_tail_tag(writer, &self.tag)?;
 
@@ -28,11 +33,12 @@ impl PresentationProperties {
   pub fn new() -> PresentationProperties {
     PresentationProperties {
       tag: "presentationPr".to_string(),
+      xmlns: XmlNamespace::Presentation,
     }
   }
 
   pub fn write(&self, writer: &mut io::BufWriter<fs::File>) -> Result<(), io::Error> {
-    super::write_head_tag(writer, &self.tag)?;
+    super::write_head_tag(writer, &self.tag, &self.xmlns)?;
 
     super::write_tail_tag(writer, &self.tag)?;
 
